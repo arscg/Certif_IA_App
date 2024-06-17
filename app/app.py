612e97ecb19c0ac9a81ad3b51fbbc902dc213720
ars <arscaldgral@gmail.com>
2024-06-17 17:51:39 +0200

    test 5

--- app/app.py
+++ app/app.py
@@ -1,24 +1,24 @@
 from flask import Flask
 import tracemalloc
 from flask_jwt_extended import JWTManager
-from routes import configure_routes
+# from routes import configure_routes
 from flask_restx import Api
-from database import db_manager
+# from database import db_manager
 
 app = Flask(__name__)
 
-# Récupération de la clé JWT depuis la base de données
-jwt_key = db_manager.get_jwt_key()
-if jwt_key is None:
-    jwt_key = 'fallback_secret_key'
-app.config['JWT_SECRET_KEY'] = jwt_key
+# # Récupération de la clé JWT depuis la base de données
+# jwt_key = db_manager.get_jwt_key()
+# if jwt_key is None:
+#     jwt_key = 'fallback_secret_key'
+# app.config['JWT_SECRET_KEY'] = jwt_key
 
-jwt = JWTManager(app)
+# jwt = JWTManager(app)
 
-api = Api(app, version='1.0', title='Animal Behavior API',
-          description='A simple API for managing animal behavior data')
+# api = Api(app, version='1.0', title='Animal Behavior API',
+#           description='A simple API for managing animal behavior data')
 
-configure_routes(app, api)
+# configure_routes(app, api)
 
 tracemalloc.start()
 
